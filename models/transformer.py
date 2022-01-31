892a0331bd81d2dac478d675c5d20607535e2f5e
wangjian123799 <70125266+wangjian123799@users.noreply.github.com>
2022-01-31 08:46:43 +0800

    Update transformer.py

--- models/transformer.py
+++ models/transformer.py
@@ -3,9 +3,9 @@
 DETR Transformer class.
 
 Copy-paste from torch.nn.Transformer with modifications:
-    * positional encodings are passed in MHattention     #位置编码被传递给MHattention
-    * extra LN at the end of encoder is removed       #编码器末端多余的LN被移除
-    * decoder returns a stack of activations from all decoding layers   #解码器返回来自所有解码层的激活堆栈
+    * positional encodings are passed in MHattention     
+    * extra LN at the end of encoder is removed       
+    * decoder returns a stack of activations from all decoding layers   
 """
 import copy
 from typing import Optional, List
@@ -42,18 +42,18 @@ class Transformer(nn.Module):
         self.d_model = d_model
         self.nhead = nhead
 
-    def _reset_parameters(self):#重置参数
+    def _reset_parameters(self):
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)  #xavier初始化（均匀分布）
 
-    def forward(self, src, mask, query_embed, pos_embed):  # query_embed问询嵌入，pos_embed位置嵌入
-        # flatten NxCxHxW to HWxNxC   #四维到三维
-        # 这里的C与hidden_dim相等
+    def forward(self, src, mask, query_embed, pos_embed):  
+        # flatten NxCxHxW to HWxNxC   
+     
         bs, c, h, w = src.shape
-        src = src.flatten(2).permute(2, 0, 1)  # flatten实现扁平化，permute将tensor的维度换位
+        src = src.flatten(2).permute(2, 0, 1)  
         pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
-        query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)  # unsqueeze增加一个维度，repeats:每个元素重复的次数
+        query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1) 
         mask = mask.flatten(1)
 
         tgt = torch.zeros_like(query_embed)
@@ -61,8 +61,7 @@ class Transformer(nn.Module):
 
         hs = self.decoder(tgt, memory, memory_key_padding_mask=mask,
                           pos=pos_embed, query_pos=query_embed)
-        # print("这是hs", hs.shape)
-        # print("这是memory", memory.shape)
+        
         return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
 
 
@@ -71,8 +70,7 @@ class TransformerEncoder(nn.Module):
     def __init__(self, encoder_layer, num_layers, norm=None):
         super().__init__()
 
-        #Encoder层，默认有6层
-        # _get_clones是对相同结构的模块进行复制，返回一个nn.ModuleList实例
+       
         self.layers = _get_clones(encoder_layer, num_layers)
         self.num_layers = num_layers
         # 归一层
@@ -82,9 +80,7 @@ class TransformerEncoder(nn.Module):
                 mask: Optional[Tensor] = None,
                 src_key_padding_mask: Optional[Tensor] = None,
                 pos: Optional[Tensor] = None):
-        # src对应backbone最后一层输出的特征图，并且维度映射到了hidden_dim,shape是（h*w，b，hidden_dim）;
-        # pos对应backbone最后一层输出的特征图对应的位置编码，shape是（h*w，b，c）
-        # src_key_padding_mask对应backbone最后一层输出的特征图对应的mask，shape是（b，h*w）
+        
         output = src
 
         for layer in self.layers:
@@ -95,7 +91,7 @@ class TransformerEncoder(nn.Module):
             output = self.norm(output)
 
         return output
-    # intermediate中记录的是每层输出后的归一化结果，而每一层的输入是前一层输出（没有归一化）的结果。
+    
 
 
 class TransformerDecoder(nn.Module):
@@ -144,7 +140,7 @@ class TransformerEncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False):
         super().__init__()
-        # 多头自注意力层
+       
         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
@@ -190,7 +186,7 @@ class TransformerDecoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False):
         super().__init__()
-        # 多头自注意力层
+  
         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
         # Encoder-Decoder Layer
         self.multihead_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
